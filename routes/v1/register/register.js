db604b24b28237e9b96591f1735e19c3e9190a0f
Manas Maheswari <manas.maheshwari26@nmims.edu.in>
2021-08-09 18:22:17 +0530

    init

--- routes/v1/register/register.js
+++ routes/v1/register/register.js
@@ -3,7 +3,7 @@ module.exports = () => {
     return (req,res) => {
         const data = req.body
         res.status(200).send({
-            'data':userService.register(data)
+            "message":"User registered"
         })
     }
 }
